22075c98ea0f220bd1b07b4792914f2681db827f
Constantine <1194941645@qq.com>
2020-03-18 18:15:57 +0800

    “第一次提交”

---
+++ src/main/java/com/qk/book/vo/User.java
@@ -0,0 +1,76 @@
+package com.qk.book.vo;
+
+public class User {
+    private int id;
+    private String name;
+    private int sex;
+    private String avatar;
+    private int status;
+    private double balance;
+    private double integer;
+    private int classVip;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getInteger() {
+        return integer;
+    }
+
+    public void setInteger(double integer) {
+        this.integer = integer;
+    }
+
+    public int getClassVip() {
+        return classVip;
+    }
+
+    public void setClassVip(int classVip) {
+        this.classVip = classVip;
+    }
+}
